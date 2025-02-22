98246f4e32417c76d0c9d94de343c776e936630e
David Maignan <davidmaignan@Davids-MacBook-Air.local>
2025-02-22 09:24:30 -0500

    Add unit test to page controller

--- test/slax_web/controllers/page_controller_test.exs
+++ test/slax_web/controllers/page_controller_test.exs
@@ -1,6 +1,20 @@
 defmodule SlaxWeb.PageControllerTest do
   use SlaxWeb.ConnCase
 
+  alias Slax.Chat.Room
+  alias Slax.Repo
+
+  @attr %{name: "the-shire", topic: "Bilbo's birthday party"}
+
+  setup do
+    user =
+      %Room{}
+      |> Room.changeset(@attr)
+      |> Repo.insert!()
+
+    {:ok, user: user}
+  end
+
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
     assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
