353629838da452fa5a87a2849850f3bcae84c2a8
David Maignan <davidmaignan@gmail.com>
2025-03-01 11:49:38 -0500

    Add seed data - clean up

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,31 +1,18 @@
 # Script for populating the database. You can run it as:
-
 #
-
 #     mix run priv/repo/seeds.exs
-
 #
-
 # Inside the script, you can read and write to any of your
-
 # repositories directly:
-
 #
-
 #     Slax.Repo.insert!(%Slax.SomeSchema{})
-
 #
-
 # We recommend using the bang functions (`insert!`, `update!`
-
 # and so on) as they will fail if something goes wrong.
 
 alias Slax.Accounts
-
 alias Slax.Chat.Room
-
 alias Slax.Chat.Message
-
 alias Slax.Repo
 
 names = [
